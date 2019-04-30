f7b0ff6970eb5d9cb54e1c4815f2d6b7552caa6c
softwareguycoder <softwareguycoder@gmail.com>
2019-04-30 08:49:08 -0400

    Removing outputs

--- common_core/Debug/src/common_core.d
+++
@@ -1,7 +0,0 @@
-src/common_core.o: ../src/common_core.c \
- /home/bhart/git/common_core/common_core/include/stdafx.h \
- /home/bhart/git/common_core/common_core/include/common_core.h
-
-/home/bhart/git/common_core/common_core/include/stdafx.h:
-
-/home/bhart/git/common_core/common_core/include/common_core.h:
--- common_core/Debug/src/stdafx.d
+++
@@ -1,4 +0,0 @@
-src/stdafx.o: ../src/stdafx.c \
- /home/bhart/git/common_core/common_core/include/stdafx.h
-
-/home/bhart/git/common_core/common_core/include/stdafx.h:
